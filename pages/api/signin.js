015a1beed582c48667aec2c41c06bf630b672ace
Nicole Reed <nicolereed@Nicoles-MacBook-Air.local>
2021-06-30 13:38:52 +0200

    deleted server folder and moved sendEmail into signin page

--- pages/api/signin.js
+++ pages/api/signin.js
@@ -1,9 +1,18 @@
 import { User } from '../../models/User'
 import connectDB from '../../middleware/mongodb'
 import { randomDigits } from 'crypto-secure-random-digit'
-import { sendEmail } from '../../server/sendEmail'
 import { BadRequestError } from '../../errors/badRequest.error'
 import { NotFoundError } from '../../errors/notFound.error'
+import nodemailer from 'nodemailer'
+import { google } from 'googleapis'
+
+
+const oAuth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URI
+)
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
 
 
 const handler = async (req, res) => {
@@ -40,3 +49,36 @@ const handler = async (req, res) => {
 }
 
 export default connectDB(handler)
+
+
+async function sendEmail(email, loginCode) {
+    try {
+        const accessToken = await oAuth2Client.getAccessToken()
+
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: 'fromthedeskofnicreed@gmail.com',
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: accessToken
+            }
+        })
+
+        await transport.sendMail({
+            from: 'fromthedeskofnicreed@gmail.com',
+            to: email,
+            subject: 'Your login code.',
+            text: 'Here is your login code:',
+            html: `
+            <h2>Please Use The Login Code Below To Sign Into Your Account</h2>
+            <h1>Your Code: ${loginCode}</h1>
+            `,
+        })
+
+    } catch (error) {
+        return error
+    }
+}
